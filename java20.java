cc6e8c317bc81de46f6f47075112def098afe6fe
Ptunc <spolentunc@gmail.com>
2022-10-21 12:08:04 +0300

    just commit

---
+++ java20.java
@@ -0,0 +1,20 @@
+package Java20;
+
+import java.util.Scanner;
+
+import javax.swing.text.PlainView;
+
+public class java20 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int x,y, b=1, c=1, a=1, z;
+        System.out.print("2 sayı girin: ");
+        x = input.nextInt();
+        y = input.nextInt();
+        //z = x <= y ? y-x : x-y;
+        for(int i=1; i<=x; i++) b *=i;
+        for(int j=1; j<=y; j++) c *=j;
+        for(int k=1; k<=z; k++) a *=k;
+        System.out.println("kombinasyonu: " + (b/(c*a)));
+    }
+}
